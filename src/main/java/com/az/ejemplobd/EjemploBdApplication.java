21ba35a45e45643be5f6ff776fe30873863d31cf
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-16 20:35:53 -0600

    Estado inicial, Spring Initialzr + dependencias JSP

---
+++ src/main/java/com/az/ejemplobd/EjemploBdApplication.java
@@ -0,0 +1,13 @@
+package com.az.ejemplobd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploBdApplication.class, args);
+	}
+
+}
