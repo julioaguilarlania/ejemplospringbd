61883e14591d323731809c13ffcc6777019b0b6d
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-23 15:43:25 -0600

    CRUD basico en REST

--- src/main/java/com/az/ejemplobd/controladores/ControladorRestVehiculos.java
+++ src/main/java/com/az/ejemplobd/controladores/ControladorRestVehiculos.java
@@ -4,10 +4,14 @@ import com.az.ejemplobd.entidades.Vehiculo;
 import com.az.ejemplobd.repositorios.RepositorioVehiculos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ControladorRestVehiculos {
@@ -23,5 +27,58 @@ public class ControladorRestVehiculos {
     public List<Vehiculo> getTodos() {
         LOGGER.debug("GET vehiculos");
         return this.repoVehiculos.findAll();
+    }
+
+    @GetMapping("vehiculos/{placas}")
+    public ResponseEntity<Vehiculo> getPorPlacas(
+            @PathVariable("placas") String p
+    ) {
+        LOGGER.debug("GET vehiculos placas:{}", p);
+        Optional<Vehiculo> opv = this.repoVehiculos.findById(p);
+        if (opv.isPresent()) {
+            return ResponseEntity.ok(opv.get());
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("vehiculos")
+    public ResponseEntity<Vehiculo> crear(
+            @RequestBody Vehiculo nuevoV
+    ) {
+       try {
+           this.repoVehiculos.save(nuevoV);
+           return ResponseEntity
+                   .created(new URI("vehiculos/" + nuevoV.getPlacas()))
+                   .body(nuevoV);
+       }
+       catch (Exception ex) {
+           return ResponseEntity.status(400)
+                   .header("ERROR", ex.getMessage())
+                   .build();
+       }
+    }
+
+    @PutMapping("vehiculos/{placas}")
+    public ResponseEntity<Vehiculo> actualizar(
+            @RequestBody Vehiculo v,
+            @PathVariable("placas") String p) {
+        try {
+            v.setPlacas(p);
+            this.repoVehiculos.save(v);
+            return ResponseEntity.ok(v);
+        }
+        catch (Exception ex) {
+            return ResponseEntity.status(400)
+                    .header("ERROR", ex.getMessage())
+                    .build();
+        }
+    }
+
+    @DeleteMapping("vehiculos/{placas}")
+    public ResponseEntity borrar(@PathVariable("placas") String p) {
+        this.repoVehiculos.deleteById(p);
+        return ResponseEntity.ok().build();
     }
 }
